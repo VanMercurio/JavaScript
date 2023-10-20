751717277089d147a75420bde4475d21d299a433
Vanessa Mercurio <142271576+VanMercurio@users.noreply.github.com>
2023-10-20 09:55:24 -0300

    Aprendendo JavaScript
    Exercicios e anotações

---
+++ JavaScript/nodearray.js
@@ -0,0 +1,14 @@
+var alunos = {"Cadastro" : [
+    {"cep": "13035-270",},
+    {"logradouro": "Rua Doutor Sales de Oliveira"},
+    {"complemento": "até 1750/1751"},
+    {"bairro": "Vila Industrial"},
+    {"localidade": "Campinas"},
+    {"uf": "SP"},
+    {"ibge": "3509502"},
+    {"gia": "2446"},
+    {"ddd": "19"},
+    {"siafi": "6291",}
+
+]
+}
